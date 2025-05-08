c775a61868e1627921f372777efea329a7c01935
alietran <alietran0211@gmail.com>
2025-05-08 22:43:46 +0700

    connect mongodb & config env

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import userRouter from './routes/users.routes'
+import databaseService from './services/database.service'
 const app = express()
 const port = 3000
 
@@ -7,6 +8,7 @@ const port = 3000
 app.use(express.json())
 
 app.use('/users', userRouter)
+databaseService.connect()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
